9c450e85bf1ec6b49b14d2ec46a824009f70f773
Juan Fazzini <jfazzini@makingsense.com>
2020-10-07 09:01:40 -0500

    [PRM-31] Add GitHub Actions CI (#7)
    * Removed latest docker tag

---
+++ src/PrecisionReporters.Platform/PrecisionReporters.Platform.UnitTests/PlaceHolderTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace PrecisionReporters.Platform.UnitTests
+{
+    public class PlaceHolderTests
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
