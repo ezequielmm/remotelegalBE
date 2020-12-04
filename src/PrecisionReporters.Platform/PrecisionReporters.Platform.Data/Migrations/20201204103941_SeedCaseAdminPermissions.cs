5e817e74f7f99b92dff53b574e86be2d8cc701fd
Martin Keimel <martinkeimel@gmail.com>
2020-12-04 11:53:13 -0300

    [PRM-789] Seed case admin roles (#94)

---
+++ src/PrecisionReporters.Platform/PrecisionReporters.Platform.Data/Migrations/20201204103941_SeedCaseAdminPermissions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrecisionReporters.Platform.Data.Migrations
+{
+    public partial class SeedCaseAdminPermissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Custom SQL for inserting permissions for existing cases
+            migrationBuilder.Sql("INSERT INTO UserResourceRoles (RoleId, UserId, ResourceId, ResourceType) " +
+                "SELECT 'c7f87850-e176-4865-b26b-cedac420a0c8', AddedById, Id, 'Case' FROM Cases WHERE AddedById IS NOT NULL;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
