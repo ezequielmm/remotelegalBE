7f83a7efedd6b0fcb0d0495426f2fec4f206d96d
noesc <noe.scordoba@gmail.com>
2021-02-25 17:32:01 -0300

    [PRM-2045] Fix update for DocumentType (#229)

---
+++ src/PrecisionReporters.Platform/PrecisionReporters.Platform.Data/Migrations/20210225183915_FixDocumentTypeUpdate.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrecisionReporters.Platform.Data.Migrations
+{
+    public partial class FixDocumentTypeUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Documents doc SET DocumentType = 'Exhibit' WHERE DocumentType = '0'");
+            migrationBuilder.Sql("UPDATE Documents doc SET DocumentType = 'Caption' WHERE DocumentType = '1'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
