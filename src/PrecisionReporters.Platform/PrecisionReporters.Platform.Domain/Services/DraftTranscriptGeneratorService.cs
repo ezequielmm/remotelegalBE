726d00b174d9618eb91f0691fc75569fb3185c6e
eszturmaj-makingsense <75989913+eszturmaj-makingsense@users.noreply.github.com>
2021-03-22 15:23:29 -0300

    Handle no witness scenario (#305)

--- src/PrecisionReporters.Platform/PrecisionReporters.Platform.Domain/Services/DraftTranscriptGeneratorService.cs
+++ src/PrecisionReporters.Platform/PrecisionReporters.Platform.Domain/Services/DraftTranscriptGeneratorService.cs
@@ -175,7 +175,7 @@ namespace PrecisionReporters.Platform.Domain.Services
             replacer.AddString("dd_tmp", deposition.StartDate.Day.ToString());
             replacer.AddString("yyyy_tmp", deposition.StartDate.Year.ToString());
             replacer.AddString("time_tmp", ConvertTimeZone(deposition.StartDate, deposition.TimeZone));
-            replacer.AddString("witness_tmp", deposition.Participants?.FirstOrDefault(x => x.Role == ParticipantType.Witness)?.Name);
+            replacer.AddString("witness_tmp", deposition.Participants?.FirstOrDefault(x => x.Role == ParticipantType.Witness)?.Name ?? "");
             replacer.AddString("reportedBy_tmp", $"{deposition.Requester.FirstName} {deposition.Requester.LastName}");
             replacer.AddString("job_n_tmp", deposition.Job ?? string.Empty);
             replacer.Process(page1);
