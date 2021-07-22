c6422427f2f5ae7d225c1496aa9513e7ebb50864
Ezequiel <ezequielmora1986@gmail.com>
2021-07-22 18:32:23 -0300

    [PRM 3867] Add new action for existing resourceType
    * Created new ressource action
    * Created new SeedRoles
    * Created new Role
    * Use same resource type: Deposition

--- src/PrecisionReporters.Platform/PrecisionReporters.Platform.Data/Enums/ResourceAction.cs
+++ src/PrecisionReporters.Platform/PrecisionReporters.Platform.Data/Enums/ResourceAction.cs
@@ -16,6 +16,7 @@
         AdmitParticipants,
         ReSchedule, 
         Notify,
-        List
+        List,
+        ViewDepositionStatus
     }
 }
---
+++ src/PrecisionReporters.Platform/PrecisionReporters.Platform.Data/Migrations/20210722142544_addtechexpert.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrecisionReporters.Platform.Data.Migrations
+{
+    public partial class addtechexpert : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "RolePermissions",
+                columns: new[] { "RoleId", "Action" },
+                values: new object[] { "6c73879b-cce3-47ea-9b80-12e1c4d1285e", "ViewDepositionStatus" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { "ee816afa-0399-472d-947f-73bfcb17775e", "DepositionTechExpert" });
+
+            migrationBuilder.InsertData(
+                table: "RolePermissions",
+                columns: new[] { "RoleId", "Action" },
+                values: new object[,]
+                {
+                    { "ee816afa-0399-472d-947f-73bfcb17775e", "EndDeposition" },
+                    { "ee816afa-0399-472d-947f-73bfcb17775e", "Recording" },
+                    { "ee816afa-0399-472d-947f-73bfcb17775e", "ViewSharedDocument" },
+                    { "ee816afa-0399-472d-947f-73bfcb17775e", "StampExhibit" },
+                    { "ee816afa-0399-472d-947f-73bfcb17775e", "View" },
+                    { "ee816afa-0399-472d-947f-73bfcb17775e", "AdmitParticipants" },
+                    { "ee816afa-0399-472d-947f-73bfcb17775e", "ViewDepositionStatus" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RolePermissions",
+                keyColumns: new[] { "RoleId", "Action" },
+                keyValues: new object[] { "6c73879b-cce3-47ea-9b80-12e1c4d1285e", "ViewDepositionStatus" });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermissions",
+                keyColumns: new[] { "RoleId", "Action" },
+                keyValues: new object[] { "ee816afa-0399-472d-947f-73bfcb17775e", "View" });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermissions",
+                keyColumns: new[] { "RoleId", "Action" },
+                keyValues: new object[] { "ee816afa-0399-472d-947f-73bfcb17775e", "EndDeposition" });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermissions",
+                keyColumns: new[] { "RoleId", "Action" },
+                keyValues: new object[] { "ee816afa-0399-472d-947f-73bfcb17775e", "Recording" });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermissions",
+                keyColumns: new[] { "RoleId", "Action" },
+                keyValues: new object[] { "ee816afa-0399-472d-947f-73bfcb17775e", "ViewSharedDocument" });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermissions",
+                keyColumns: new[] { "RoleId", "Action" },
+                keyValues: new object[] { "ee816afa-0399-472d-947f-73bfcb17775e", "StampExhibit" });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermissions",
+                keyColumns: new[] { "RoleId", "Action" },
+                keyValues: new object[] { "ee816afa-0399-472d-947f-73bfcb17775e", "AdmitParticipants" });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermissions",
+                keyColumns: new[] { "RoleId", "Action" },
+                keyValues: new object[] { "ee816afa-0399-472d-947f-73bfcb17775e", "ViewDepositionStatus" });
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "ee816afa-0399-472d-947f-73bfcb17775e");
+        }
+    }
+}
