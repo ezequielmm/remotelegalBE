382d165799b5c1bc5722cef83341a4d8054014b7
Gonzalo Romero <71275081+gromero-ms@users.noreply.github.com>
2021-07-23 14:52:25 -0300

    [NO-TICKET] Add decorators in TranscriptionHub
    - Add dataannotations on TranscriptionsHubDto and TranscriptionsChangeStatusDto

--- src/PrecisionReporters.Platform/PrecisionReporters.Platform.Domain/Dtos/TranscriptionsChangeStatusDto.cs
+++ src/PrecisionReporters.Platform/PrecisionReporters.Platform.Domain/Dtos/TranscriptionsChangeStatusDto.cs
@@ -1,10 +1,16 @@
-﻿using MessagePack;
+﻿using System;
+using MessagePack;
+using PrecisionReporters.Platform.Data.Enums;
+using PrecisionReporters.Platform.Domain.Attributes;
 
 namespace PrecisionReporters.Platform.Domain.Dtos
 {
     [MessagePackObject]
     public class TranscriptionsChangeStatusDto
     {
+        [Key("depositionId")]
+        [ResourceId(ResourceType.Deposition)]
+        public Guid DepositionId { get; set; }
         [Key("offRecord")]
         public bool OffRecord { get; set; }
     }
