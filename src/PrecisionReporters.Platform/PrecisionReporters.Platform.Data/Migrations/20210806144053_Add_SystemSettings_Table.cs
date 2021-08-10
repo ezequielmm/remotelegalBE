ee8577911ed73c170fc287ca4db40dfe873ba1c4
fernandoserna-makingsense <78917471+fernandoserna-makingsense@users.noreply.github.com>
2021-08-10 13:13:04 -0500

    Changed seed values for system settings table (#552)

--- src/PrecisionReporters.Platform/PrecisionReporters.Platform.Data/Migrations/20210806144053_Add_SystemSettings_Table.cs
+++ src/PrecisionReporters.Platform/PrecisionReporters.Platform.Data/Migrations/20210806144053_Add_SystemSettings_Table.cs
@@ -23,9 +23,9 @@ namespace PrecisionReporters.Platform.Data.Migrations
                     table.PrimaryKey("PK_SystemSettings", x => x.Id);
                 });
 
-            migrationBuilder.Sql("INSERT INTO SystemSettings(Id,CreationDate,Name,Value)VALUES('914e50de-09b9-40d2-bf9e-aa828946e183',CURRENT_TIMESTAMP,'EnableLiveTranscriptions','true')");
-            migrationBuilder.Sql("INSERT INTO SystemSettings(Id,CreationDate,Name,Value)VALUES('dd502081-525a-4faa-a9aa-3de98e4368e7',CURRENT_TIMESTAMP,'EnableBreakrooms','true');");
-            migrationBuilder.Sql("INSERT INTO SystemSettings(Id,CreationDate,Name,Value)VALUES('edbc658a-1d82-4f2f-9e6e-697d0ab0cfe1',CURRENT_TIMESTAMP,'EnableRealTimeTab','true');");
+            migrationBuilder.Sql("INSERT INTO SystemSettings(Id,CreationDate,Name,Value)VALUES('914e50de-09b9-40d2-bf9e-aa828946e183',CURRENT_TIMESTAMP,'EnableLiveTranscriptions','enabled')");
+            migrationBuilder.Sql("INSERT INTO SystemSettings(Id,CreationDate,Name,Value)VALUES('dd502081-525a-4faa-a9aa-3de98e4368e7',CURRENT_TIMESTAMP,'EnableBreakrooms','enabled');");
+            migrationBuilder.Sql("INSERT INTO SystemSettings(Id,CreationDate,Name,Value)VALUES('edbc658a-1d82-4f2f-9e6e-697d0ab0cfe1',CURRENT_TIMESTAMP,'EnableRealTimeTab','enabled');");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
