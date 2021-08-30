4700777a5fba219fd2b380c51f1471a1cdc6b14f
Ezequiel <ezequielmora1986@gmail.com>
2021-08-30 14:47:39 -0300

    [PRM 4175] New system Setting for Enable and disable twilio logs

---
+++ src/PrecisionReporters.Platform/PrecisionReporters.Platform.Data/Migrations/20210823182107_addNewSystemSettingsPropertywithEnabled.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrecisionReporters.Platform.Data.Migrations
+{
+    public partial class addNewSystemSettingsPropertywithEnabled : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO SystemSettings(Id,CreationDate,Name,Value)VALUES('8e44479e-8581-49c8-82e4-20cdd47b54d4',CURRENT_TIMESTAMP,'EnableTwilioLogs','enabled')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
