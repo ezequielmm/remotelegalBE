c833c268d27a01346902bf4edbad714b32a3d500
waguilarms <87650490+waguilarms@users.noreply.github.com>
2021-09-27 09:34:13 -0300

    [PRM-4435] Add Docker Compose to Setup BE environment (#646)

---
+++ src/temp/database/init.sql
@@ -0,0 +1,7 @@
+USE `RemoteLegal-Dev`
+
+CREATE TABLE `__EFMigrationsHistory` (
+  `MigrationId` varchar(150) NOT NULL,
+  `ProductVersion` varchar(32) NOT NULL,
+  PRIMARY KEY (`MigrationId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
