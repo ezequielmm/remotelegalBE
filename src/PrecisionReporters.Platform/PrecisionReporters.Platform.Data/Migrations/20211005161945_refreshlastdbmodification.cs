4494d45588496fb6191188b6f09dffd81324595b
Gonzalo Romero <71275081+gromero-ms@users.noreply.github.com>
2021-10-05 13:31:43 -0300

    [NO-TICKET] Add migration for UserResourceRoles

---
+++ src/PrecisionReporters.Platform/PrecisionReporters.Platform.Data/Migrations/20211005161945_refreshlastdbmodification.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrecisionReporters.Platform.Data.Migrations
+{
+    public partial class refreshlastdbmodification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "UserResourceRoles",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "UserResourceRoles");
+        }
+    }
+}
